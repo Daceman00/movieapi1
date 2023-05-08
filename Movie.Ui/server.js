fb4db6311726cd06096abf0e294a32b291010a13
Dario Mandic <dariomandic2000@gmail.com>
2023-05-08 15:18:19 +0200

    updates to deploy heroku

--- Movie.Ui/server.js
+++ Movie.Ui/server.js
@@ -10,10 +10,28 @@ const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./dist/movie.ui/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/movie.ui/'}),
+);
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+const express = require('express');
+const app = express();
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/movie.ui'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/movie.ui/'}),
 );
 
 app.listen(process.env.PORT || 8080);
